9da7cc431484f1c444fd9783b9b5bcf008a89beb
Nikhil Kumar <knikhil02004@gmail.com>
2025-07-09 21:44:30 +0530

    Added backend structure

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+// backend/index.js
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const exampleRoutes = require('./routes/example');
+app.use('/api/example', exampleRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
